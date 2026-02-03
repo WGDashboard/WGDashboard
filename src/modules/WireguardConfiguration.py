b84d9f6e1344020700add4d0d9e851eb144f55db
Mikhail Solovev <mike@solovjov.net>
2026-02-03 18:23:36 +0300

    Update WireguardConfiguration.py
    wrong symbol

--- src/modules/WireguardConfiguration.py
+++ src/modules/WireguardConfiguration.py
@@ -791,7 +791,7 @@ class WireguardConfiguration:
                         cur_total_receive = float(data_usage[i][1]) / (1024 ** 3)
                         cumulative_receive = cur_i['cumu_receive'] + total_receive
                         cumulative_sent = cur_i['cumu_sent'] + total_sent
-                        if (total_sent * 0.999 ) <= cur_total_sent and (total_receive * ).999) <= cur_total_receive: # An accuracy of 1K ppm is sufficient
+                        if (total_sent * 0.999 ) <= cur_total_sent and (total_receive * 0.999) <= cur_total_receive: # An accuracy of 1K ppm is sufficient
                             total_sent = cur_total_sent
                             total_receive = cur_total_receive
                         else:
